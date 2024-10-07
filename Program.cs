f2c59385118c0c43bc263a3e97776120e6b50e2b
csoke.balazs <csoke.balazs@28-DIAK9>
2024-10-07 15:17:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using doga20241007;
+using System.Text;
+
+List<Verseny> versenyek = [];
+
+StreamReader sr = new StreamReader(@"..\..\..\src\forras.txt", encoding: System.Text.Encoding.UTF8);
+
+while (!sr.EndOfStream)
+{
+    versenyek.Add(new Verseny(sr.ReadLine()));
+}
+
+Console.WriteLine($"{versenyek.Count} db versenyző fejezte be a versenyt");
+
+//select nev.Count from Verseny where korcsopport = 25-29
+var f2 = versenyek.Count(v => v.Korcsoport == "25-29");
+Console.WriteLine($"{f2} darab versenyző van a 25-29 közötti korcsoportban.");
+
+var f3 = versenyek.Average(p => p.Szuletesiev);
+Console.WriteLine($"{f3} az átlag életkor");
+
+double f4 = Math.Round(versenyek.Sum(p => p.Idoadatok["kerekpar"].TotalHours),2);
+Console.WriteLine($"{f4} az uszással töltött idő");
+
+var f5a =  versenyek.Where(p => p.Korcsoport == "elit");
+double f5b = f5a.Average(p => p.Idoadatok["uszasido"].TotalSeconds);
+Console.WriteLine($"{f5b} az átlagos úszási idő elit kategóriában");
+
+var ferfi = versenyek.Where(p => p.Nem == "f");
+var f6 = ferfi.MinBy(p => p.Idoadatok.Sum(x => x.Value.TotalSeconds));
+Console.WriteLine($"{f6} férfi győztes van");
+
+var f7a = versenyek.GroupBy(p => p.Korcsoport);
+foreach(var kategoria in f7a)
+{
+    Console.WriteLine($"{kategoria.Key}: {kategoria.Count()}");
+    Console.WriteLine($"Átlagos depóban töltött idő: {kategoria.Average(p => p.Idoadatok["elsodepo"].TotalSeconds + p.Idoadatok["masodikdepo"].TotalSeconds)}");
+}
---
+++ Verseny.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doga20241007
+{
+    internal class Verseny
+    {
+        public string Nev {  get; set; }
+        public int Szuletesiev {  get; set; }
+        public int Rajtszam {  get; set; }
+        public string Nem {  get; set; }
+        public string Korcsoport { get; set; }
+        public Dictionary<string, TimeSpan> Idoadatok = new Dictionary<string, TimeSpan>();
+
+        public override string ToString()
+        {
+            return
+                $"\tnev: {Nev}\n" +
+                $"\tszuletesi ev: {Szuletesiev}\n" +
+                $"\trajtszama: {Rajtszam}\n" +
+                $"\tneme {Nem}\n" +
+                $"\tkorcsoport: {Korcsoport}\n" +
+                $"\tidoadatai: \n" +
+                $"\t{Idoadatok["uszasido"]}\n" +
+                $"\t{Idoadatok["elsodepo"]}\n" +
+                $"\t{Idoadatok["kerekpar"]} \n" +
+                $"\t{Idoadatok["masodikdepo"]}\n" +
+                $"\t{Idoadatok["futasido"]}\n";
+        }
+        public Verseny(string row)
+        {
+            var v = row.Split(';');
+            Nev = v[0];
+            Szuletesiev = int.Parse(v[1]);
+            Rajtszam = int.Parse(v[2]);
+            Nem = v[3];
+            Korcsoport = v[4];
+            Idoadatok.Add("uszasido", TimeSpan.Parse(v[5]));
+            Idoadatok.Add("elsodepo", TimeSpan.Parse(v[6]));
+            Idoadatok.Add("kerekpar", TimeSpan.Parse(v[7]));
+            Idoadatok.Add("masodikdepo", TimeSpan.Parse(v[8]));
+            Idoadatok.Add("futasido", TimeSpan.Parse(v[9]));
+        }
+    }
+}
